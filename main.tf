49346310aeb1bf52f3572ef49f95e94dd798755e
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-11-17 12:57:14 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "vpc" {
   name = "sam-demo-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = []
+  azs             = ["${var.region}a","${var.region}b","${var.region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
